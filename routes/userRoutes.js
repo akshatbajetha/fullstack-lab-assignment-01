5d4584e92a7dc73747cfce2037fe6abbd6640b98
Akshat Bajetha <akshatbajetha@gmail.com>
2024-09-25 21:59:09 +0530

    Updated /users routes

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -3,8 +3,9 @@ const userController = require("../controllers/userController");
 
 const router = express.Router();
 
-router.route("/signup").post(userController.createUser).get(userController.getAllUsers);
-router.route("/signup/:username").get(userController.getUserByUsername);
+router.route("/signup").post(userController.createUser)
+router.route("/users").get(userController.getAllUsers);
+router.route("/users/:username").get(userController.getUserByUsername);
 router.route("/delete/:username").delete(userController.deleteUserByUsername);
 
 module.exports = router;
